c7217ee88ce51dc88d4c9f5413cd6533dd251cbb
HengSophavong <hengsophavong@gmail.com>
2022-10-21 07:49:05 +0700

    GG I GUESS

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Product;
 
 
 class AdminController extends Controller
@@ -25,5 +26,67 @@ class AdminController extends Controller
         $data=Category::find($id);
         $data->delete();
         return redirect()->back()->with('message','Category Deleted Successfully');
+    }
+    public function view_product(){
+        $category=category::all();
+        return view('admin.product', compact('category'));
+    }
+    public function add_product(Request $request){
+        $product=new Product;
+
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->category = $request->category;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+        $product->discount_price = $request->dis_price;
+        
+        $image=$request->image;
+
+        $imagename=time().'.'.$image->getClientOriginalExtension();
+        
+        $request->image->move('product',$imagename);
+
+        $product->image=$imagename;
+
+        $product->save();
+        return redirect()->back()->with('message','Product Added Successfully');
+    }
+    public function show_product(){
+        $product=Product::all();
+        return view('admin.show_product', compact('product'));
+    }
+    public function delete_product($id){
+        $product=Product::find($id);
+        $product->delete();
+        return redirect()->back()->with('message','Product Deleted Successfully');
+    }
+    public function update_product($id){
+        $product=Product::find($id);
+        $category=Category::all();
+        return view('admin.update_product', compact('product','category'));
+    }
+    public function update_product_confirm($id, Request $request){
+        $product=Product::find($id);
+
+        $product->title = $request->title;
+        $product->description = $request->description;
+        $product->category = $request->category;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+        $product->discount_price = $request->dis_price;
+        
+        $image=$request->image;
+
+        if($image){
+            $imagename=time().'.'.$image->getClientOriginalExtension();
+            
+            $request->image->move('product',$imagename);
+
+            $product->image=$imagename;
+        }
+
+        $product->save();
+        return redirect()->back()->with('message','Product Updated Successfully');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use App\Http\Controllers\AdminController;
 */
 route::get('/',[HomeController::class, 'index'] );
 
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -30,4 +32,14 @@ route::get('/redirect',[HomeController::class, 'redirect'] );
 route::get('/view_category',[AdminController::class, 'view_category'] );
 route::post('/add_category',[AdminController::class, 'add_category'] );
 route::get('/delete_category/{id}',[AdminController::class, 'delete_category'] );
+route::get('/view_product',[AdminController::class, 'view_product'] );
+route::post('/add_product',[AdminController::class, 'add_product'] );
+route::get('/show_product',[AdminController::class, 'show_product'] );
+route::get('/delete_product/{id}',[AdminController::class, 'delete_product'] );
+route::get('/update_product/{id}',[AdminController::class, 'update_product'] );
+route::post('/update_product_confirm/{id}',[AdminController::class, 'update_product_confirm'] );
+route::get('/product_details/{id}',[HomeController::class, 'product_details'] );
+route::post('/add_cart/{id}',[HomeController::class, 'add_cart'] );
+route::get('/show_cart',[HomeController::class, 'show_cart'] );
+route::get('/remove_cart/{id}',[HomeController::class, 'remove_cart'] );
 
